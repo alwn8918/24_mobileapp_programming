580b964703cb2509d70c9835058bcc1868868832
miju <alwn8918@gmail.com>
2024-06-17 08:27:02 +0900

    드로어 레이아웃

--- app/src/main/java/com/example/finalapplication/MainActivity.kt
+++ app/src/main/java/com/example/finalapplication/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.finalapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import com.example.finalapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    lateinit var toggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // action bar에 토글 버튼 추가
+        toggle = ActionBarDrawerToggle(this, binding.drawer, R.string.drawer_opened, R.string.drawer_closed)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toggle.syncState()
+    }
+
+    // 토글 버튼 누르면 드로어 화면 나타남
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+
+        when(item.itemId) {
+            R.id.login -> {
+                true
+            }
+            R.id.heart -> {
+                true
+            }
+            R.id.myComment -> {
+                true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
